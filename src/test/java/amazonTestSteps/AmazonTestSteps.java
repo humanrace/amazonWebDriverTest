796c363d6800b48e80a60a39802d64774df68a66
PavelFokin <humanrace@mail.ru>
2017-05-13 23:44:35 +0300

    3 steps implemented

--- src/test/java/amazonTestSteps/AmazonTestSteps.java
+++ src/test/java/amazonTestSteps/AmazonTestSteps.java
@@ -6,10 +6,16 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import org.junit.Assert;
 import org.junit.Assert.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.server.SeleniumServer;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.FileAssert.fail;
 
 /**
  * Created by pfokin on 12.05.2017.
@@ -20,7 +26,10 @@ public class AmazonTestSteps {
     private static boolean IF_CHROME_BROWSER_USED = true;
     public static WebDriver driver;
 
+    public static final String SITE_URL = "https://www.amazon.co.uk/";
+
     @Before
+
     public static void goToAmazon() {
 
         if (IF_CHROME_BROWSER_USED) {
@@ -29,9 +38,9 @@ public class AmazonTestSteps {
     }
 
     @Given("Im opening '([^\"]*)'")
-    public void openSite(String siteDomainName){
+    public void openSite(String siteDomainName) throws Exception {
 
-       // System.setProperty("webdriver.chrome.driver","D:\\Work\\chromedriver.exe");
+        // System.setProperty("webdriver.chrome.driver","D:\\Work\\chromedriver.exe");
 
 //        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 //        capabilities.setCapability("marionette", true);
@@ -41,21 +50,54 @@ public class AmazonTestSteps {
         driver.manage().window().maximize();
         driver.get(siteDomainName);
 
-        String url driver.getCurrentUrl()
-        if (driver.getCurrentUrl().isEmpty()) {
+        for (int second = 0; ; second++) {
+            if (second >= 20) fail("Unable to open " + SITE_URL + " ..check URL ar internet connection");
+            try {
+                if (driver.getTitle().contains("Amazon.co.uk")) break;
+            } catch (Exception e) {
+            }
+            Thread.sleep(1000);
+        }
 
-            driver.getCurrentUrl().equals(siteDomainName);
+        if (driver.getCurrentUrl() == null) fail("Site URL in Null..");
+
+        Assert.assertTrue("Site URL is not same as expected one..Expected: " + SITE_URL
+                        + " But Actual was: " + driver.getCurrentUrl()
+                , driver.getCurrentUrl().equals(SITE_URL));
+
+        Assert.assertTrue("Site title does not contain..Expected: Amazon.co.uk"
+                        + " But Actual was: " + driver.getTitle()
+                , driver.getTitle().contains("Amazon.co.uk"));
 
     }
 
-    @And("Verify that the page is correct and opened")
-    public void checkPageIsValid()
 
-    {
+    @And("Search for '([^\"]*)' and the '([^\"]*)' in section books")
+    public void searchForBooks(String authorName, String bookName) {
+
+
+        driver.get(SITE_URL);
+        driver.findElement(By.id("twotabsearchtextbox")).clear();
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Harry Potter and the Cursed Child");
+        driver.findElement(By.id("issDiv1")).click();
+
+        assertTrue(driver.getTitle().equalsIgnoreCase("Amazon.co.uk: Harry Potter and the Cursed Child: Books"));
+    }
+
+    @And("Verify that the first items has the title: '([^\"]*)'")
+    public void checkFirstItemOnThePage(String firstItemTitle) {
+
+            //driver.findElement(By.id("result_0")).getAttribute();
+
+        assertTrue(driver.findElement(By.xpath("//li[@id='result_0']/div/div/div/div[2]/div[2]/div/a/h2")).getText()
+                .contains(firstItemTitle),
+                "First element of the page is not same as expected one..Actual: was: "+
+                        driver.findElement(By.xpath("//li[@id='result_0']/div/div/div/div[2]/div[2]/div/a/h2")).getText()
+        );
+
 
 
     }
-
 
 
     @After
@@ -65,4 +107,8 @@ public class AmazonTestSteps {
         driver.close();
     }
 }
+
+
+
+
 
--- src/test/resources/feature/AmazonTestIT.feature
+++ src/test/resources/feature/AmazonTestIT.feature
@@ -2,15 +2,13 @@ Feature: Simple Amazon page test
 
   Background:
 
-    And Im opening 'https://www.amazon.co.uk'
+    Given Im opening 'https://www.amazon.co.uk'
 
   Scenario: Simple Amazon page test
 
-    And Verify that the page is correct and opened
+    And Search for 'Harry Potter' and the 'Cursed Child' in section books
 
-    #And  Search for Harry Potter and the Cursed Child in section books
-
-    #And  Verify that the first items has the title: Harry Potter and the Cursed Child - Parts One & Two
+    And  Verify that the first items has the title: 'Harry Potter and The Cursed Child - Parts One and Two'
 
     #And  It has a badge “Best Seller”
 
