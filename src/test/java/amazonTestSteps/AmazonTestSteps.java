f5ff8775373d4a21cbec3c16975f255d2241b804
PavelFokin <humanrace@mail.ru>
2017-05-14 16:13:00 +0300

    all steps implemented, test passed

--- src/test/java/amazonTestSteps/AmazonTestSteps.java
+++ src/test/java/amazonTestSteps/AmazonTestSteps.java
@@ -4,14 +4,11 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import org.junit.Assert;
-import org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.server.SeleniumServer;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -23,15 +20,14 @@ import static org.testng.FileAssert.fail;
 
 public class AmazonTestSteps {
 
-    private static boolean IF_CHROME_BROWSER_USED = true;
-    public static WebDriver driver;
-
-    public static final String SITE_URL = "https://www.amazon.co.uk/";
+    private static WebDriver driver;
+    private static final String SITE_URL = "https://www.amazon.co.uk/";
+    private static final String TITLE = "Amazon.co.uk: Harry Potter and the Cursed Child: Books";
 
     @Before
-
     public static void goToAmazon() {
 
+        boolean IF_CHROME_BROWSER_USED = true;
         if (IF_CHROME_BROWSER_USED) {
             System.setProperty("webdriver.chrome.driver", "D:\\Work\\chromedriver.exe");
         }
@@ -40,12 +36,6 @@ public class AmazonTestSteps {
     @Given("Im opening '([^\"]*)'")
     public void openSite(String siteDomainName) throws Exception {
 
-        // System.setProperty("webdriver.chrome.driver","D:\\Work\\chromedriver.exe");
-
-//        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-//        capabilities.setCapability("marionette", true);
-
-        //WebDriver driver = new ChromeDriver();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(siteDomainName);
@@ -68,43 +58,141 @@ public class AmazonTestSteps {
         Assert.assertTrue("Site title does not contain..Expected: Amazon.co.uk"
                         + " But Actual was: " + driver.getTitle()
                 , driver.getTitle().contains("Amazon.co.uk"));
-
     }
 
-
-    @And("Search for '([^\"]*)' and the '([^\"]*)' in section books")
-    public void searchForBooks(String authorName, String bookName) {
-
-
+    @And("Search for '([^\"]*)' in section books")
+    public void searchForBooks(String searchString) {
         driver.get(SITE_URL);
         driver.findElement(By.id("twotabsearchtextbox")).clear();
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Harry Potter and the Cursed Child");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys(searchString);
         driver.findElement(By.id("issDiv1")).click();
 
-        assertTrue(driver.getTitle().equalsIgnoreCase("Amazon.co.uk: Harry Potter and the Cursed Child: Books"));
+        assertTrue(driver.getTitle().equalsIgnoreCase(TITLE));
     }
 
     @And("Verify that the first items has the title: '([^\"]*)'")
     public void checkFirstItemOnThePage(String firstItemTitle) {
 
-            //driver.findElement(By.id("result_0")).getAttribute();
-
         assertTrue(driver.findElement(By.xpath("//li[@id='result_0']/div/div/div/div[2]/div[2]/div/a/h2")).getText()
-                .contains(firstItemTitle),
-                "First element of the page is not same as expected one..Actual: was: "+
-                        driver.findElement(By.xpath("//li[@id='result_0']/div/div/div/div[2]/div[2]/div/a/h2")).getText()
-        );
+                        .contains(firstItemTitle),
+                "First element of the page is not same as expected one..Actual: was: " +
+                        driver.findElement(By.xpath("//li[@id='result_0']/div/div/div/div[2]/div[2]/div/a/h2")).getText());
+    }
 
+    @And("It has a badge '([^\"]*)'")
+    public void checkBadge(String badge) {
+        assertEquals(driver.findElement(By.id("result_0")).findElement(By.cssSelector(
+                "span.sx-badge-text.s-color-white")).getText(),
+                badge);
+    }
 
+    @And("Selected type is '([^\"]*)'")
+    public void checkType(String type) {
 
+        assertEquals(driver.findElement(By.id("result_0")).findElement(By.cssSelector(
+                "h3.a-size-small.s-inline.a-text-normal")).getText(),
+                type);
+    }
+
+    @And("the price is '([^\"]*)'")
+    public void checkPrice(String priceData) {
+
+        assertEquals(driver.findElement(By.id("result_0")).findElement(By.cssSelector(
+                "span.a-size-base.a-color-price.s-price.a-text-bold")).getText(),
+                priceData);
+    }
+
+    @Then("Navigate to the book details and verify the '([^\"]*)', the '([^\"]*)' the '([^\"]*)' and the type '([^\"]*)'")
+    public void navigateToBookDetails(String title, String badge, String priceData, String type) {
+
+        driver.findElement(By.id("result_0")).findElement(By.cssSelector(
+                "a.a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal")).click();
+
+        //Checking title
+        assertTrue(driver.findElement(By.id("productTitle")).getText().contains(title), "Product title doesnt contain " +
+                "expected title: Expected: " + title + " ..But Actual was:" + driver.findElement(
+                By.id("productTitle")).getText());
+        //Checking badge
+        assertTrue(driver.findElement(By.cssSelector("i.a-icon.a-icon-addon.p13n-best-seller-badge")).getText().contains(badge), "Badge does not contain expected badge data..");
+
+        //Checking priceData
+        assertEquals(driver.findElement(By.id("a-autoid-3-announce")).findElement(By.cssSelector(
+                "span.a-size-base.a-color-price.a-color-price")).getText(), priceData);
+
+        //Checking type
+        assertEquals(driver.findElement(By.id("a-autoid-3-announce")).findElement(By.cssSelector(
+                "span")).getText(), type);
     }
 
 
-    @After
-    public void closeChrome()
+    @And("Add the book to the basket")
+    public void submitToCard() {
 
-    {
+        driver.findElement(By.id("submit.add-to-cart")).click();
+    }
+
+    @And("Verify that the notification is shown with the title '([^\"]*)'")
+    public void checkNotification(String addedToBasket) {
+
+        Assert.assertTrue(driver.findElement(By.id("huc-v2-order-row-inner")).isDisplayed());
+
+        Assert.assertEquals(driver.findElement(By.id("huc-v2-order-row-inner")).findElement(By.id("huc-v2-order-row-confirm-text")).
+                getText(), addedToBasket);
+    }
+
+    @And("There is '([^\"]*)' in the basket")
+    public void checkBasketItems(String numberOfItems) {
+        Assert.assertTrue("Amount of items in the basket not same as expected..",driver.findElement(By.cssSelector(
+                "span.a-size-medium.a-align-center.huc-subtotal")).getText().
+                        contains(numberOfItems));
+
+    }
+
+    @Then("Click on edit the basket")
+    public void clickEdiBasket() {
+
+        driver.findElement(By.id("hlb-view-cart-announce")).click();
+    }
+
+    @And("Verify that the book is shown on the list")
+    public void checkBookShown() {
+
+        Assert.assertTrue("Book is not displaed in the basket list..",driver.findElement(By.cssSelector(
+                "span.a-size-medium.sc-product-title.a-text-bold")).isDisplayed());
+    }
+
+    @And("The title is '([^\"]*)', type of print is '([^\"]*)', price is '([^\"]*)', quantity is '([^\"]*)' and total price is '([^\"]*)'")
+    public void checkBasket(String title, String type, String price, String quantity, String totalPrice) {
+
+        //check Title
+        Assert.assertTrue("Title of the Book in the basket is not same as expected one....",driver.findElement(
+                By.cssSelector("span.a-size-medium.sc-product-title.a-text-bold")).getText().contains(title));
+
+        //check type
+        Assert.assertEquals("Type of the basket item is not equal to expected one..", driver.findElement(
+                By.cssSelector("span.a-size-small.a-color-secondary.sc-product-binding")).getText(),type);
+
+        //check price
+        Assert.assertEquals("Actual price of the basket product is not same as expected",price,driver.
+                findElement(By.cssSelector(
+                        "span.a-size-medium.a-color-price.sc-price.sc-white-space-nowrap.sc-product-price.sc-price-sign.a-text-bold")).
+                getText());
+
+        //check price
+        Assert.assertEquals("Quantity of expeced items in the basket is not same as expected one",quantity,
+                driver.findElement(By.id("a-autoid-2")).findElement(By.cssSelector("span.a-dropdown-prompt")).
+                getText());
+
+        //check total price
+        Assert.assertEquals("Quantity of expeced items in the basket is not same as expected one",totalPrice,
+                driver.findElement(By.id("sc-subtotal-amount-activecart")).getText());
+
+    }
+
+    @After
+    public void closeChrome() {
         driver.close();
+        driver.quit();
     }
 }
 
--- src/test/resources/feature/AmazonTestIT.feature
+++ src/test/resources/feature/AmazonTestIT.feature
@@ -4,14 +4,33 @@ Feature: Simple Amazon page test
 
     Given Im opening 'https://www.amazon.co.uk'
 
-  Scenario: Simple Amazon page test
+  Scenario Outline: Simple Amazon page test
 
-    And Search for 'Harry Potter' and the 'Cursed Child' in section books
+    And Search for 'Harry Potter and the Cursed Child' in section books
 
-    And  Verify that the first items has the title: 'Harry Potter and The Cursed Child - Parts One and Two'
+    And  Verify that the first items has the title: '<title>'
 
-    #And  It has a badge “Best Seller”
+    And  It has a badge '<badge>'
 
-    #And  Selected type is Hardcover
+    And  Selected type is '<type>'
 
-    #And the price is £9.00
+    And the price is '<price>'
+
+    Then Navigate to the book details and verify the '<title>', the '<badge>' the '<price>' and the type '<type>'
+
+    And Add the book to the basket
+
+    And Verify that the notification is shown with the title 'Added to Basket'
+
+    And There is '1 item' in the basket
+
+    Then Click on edit the basket
+
+    And Verify that the book is shown on the list
+
+    And The title is '<title>', type of print is '<type>', price is '<price>', quantity is '1' and total price is '<price>'
+
+    Examples:
+
+      | title                                                 | badge       | price | type      |
+      | Harry Potter and The Cursed Child - Parts One and Two | Best Seller | £9.00 | Hardcover |
